7091ed23464e1b16ecb11fec8a1ef3fa5af07adb
Antony-Godwin24 <antonygodwin08@gmail.com>
2025-09-30 21:38:26 +0530

    updated timeline!

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { HeroSection } from "@/components/Hero/Hero.jsx";
 import { AboutSection } from "@/components/About/About.jsx";
-import { TimelineSection } from "@/components/TimeLineSection/TimelineSection";
+import { TimelineSection } from "@/components/TimelineSection/TimelineSection";
 import { ActivitiesSection } from "@/components/ActivitiesSection/ActivitiesSection";
 import { CommunitySection } from "@/components/CommunitySection/CommunitySection";
 import { ContactSection } from "@/components/Contact/Contact";
